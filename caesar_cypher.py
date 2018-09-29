0d75c11517c181409247390a53859097654d15e2
NoName <34386898+masaya-jkl@users.noreply.github.com>
2018-09-30 01:27:05 +0200

    Documentation and POOL improvement.

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,17 +1,32 @@
 """Caesar Cypher"""
 
 from string import printable
-from typing import Callable
+from typing import Callable, List
 
-def caesar_cypher(cleartext: str, offset: int) -> str:
+POOL = printable[:-3]
+
+def caesar_cypher(cleartext: str, offset: int, pool: List[chr] = POOL) -> str:
     """Encode the cleartext with the Caesar Cypher Algorithm."""
 
     return "".join(
         map(
-            lambda x: printable[printable.find(x) + offset],
+            lambda x: POOL[(POOL.find(x) + offset) % len(POOL)],
             cleartext
             )
         )
 
-def curried_caesar_cypher(offset: int) -> Callable[[str], str]:
+def curried_caesar_cypher(offset: int, pool: List[chr] = POOL) -> Callable[[str], str]:
+    """Creates a function that encodes received cleartext with
+    the Caesar Cypher Algorithm by a fixed offset.
+
+    Useful with streams.
+
+    Usage:
+    >>> curried_caesar_cypher(5)("Hello!")
+    "Mjqqt&"
+
+    >>> f = curried_caesar_cypher(5)
+    >>> f("Hello!")
+    "Mjqqt&"
+    """
     return lambda cleartext: caesar_cypher(cleartext, offset)
