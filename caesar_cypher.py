0eb2d16869844053ed206276cf6eb07a2c73553a
NoName <34386898+masaya-jkl@users.noreply.github.com>
2018-09-30 01:16:55 +0200

    Added a curried encryption/decryption function.

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,6 +1,7 @@
 """Caesar Cypher"""
 
 from string import printable
+from typing import Callable
 
 def caesar_cypher(cleartext: str, offset: int) -> str:
     """Encode the cleartext with the Caesar Cypher Algorithm."""
@@ -11,3 +12,6 @@ def caesar_cypher(cleartext: str, offset: int) -> str:
             cleartext
             )
         )
+
+def curried_caesar_cypher(offset: int) -> Callable[[str], str]:
+    return lambda cleartext: caesar_cypher(cleartext, offset)
